ae04706b2999fe7c807c23eaedd7d81e5e32894c
Pagalavan <125946240+PagalavanPagal66@users.noreply.github.com>
2023-06-27 23:04:01 +0530

    Update CF_PREDICT.py

--- CF_PREDICT.py
+++ CF_PREDICT.py
@@ -1,12 +1,13 @@
 import ssl
 import time
+import json
+from streamlit_lottie import st_lottie
 import streamlit as st
 import pickle
 import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 import hashlib
-from streamlit_option_menu import option_menu
 import random
 import smtplib
 #from streamlit_custom_notification_box import custom_notification_box
@@ -16,6 +17,20 @@ import sqlite3
 conn = sqlite3.connect('new.db')
 c = conn.cursor()
 
+import base64
+import streamlit as st
+import plotly.express as px
+
+df = px.data.iris()
+
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("login-background (1).jpg")
 
 def make_hashes(password):
 	return hashlib.sha256(str.encode(password)).hexdigest()
@@ -26,7 +41,7 @@ def check_hashes(password,hashed_text):
 	return False
 
 def create_usertable():
-	c.execute('CREATE TABLE final (username TEXT NOT NULL UNIQUE,password TEXT NOT NULL UNIQUE,mail TEXT NOT NULL UNIQUE);')
+	c.execute('CREATE TABLE IF NOT EXISTS final (username TEXT NOT NULL UNIQUE,password TEXT NOT NULL UNIQUE,mail TEXT NOT NULL UNIQUE);')
 	conn.commit()
 def add_userdata(username,password,mail):
 	c.execute('INSERT INTO final (username,password,mail) VALUES (?,?,?)',(username,password,mail))
@@ -34,7 +49,7 @@ def add_userdata(username,password,mail):
 
 def add_feedback(username,feedback):
 	now = datetime.datetime.now()
-	c.execute('INSERT INTO fbf (username,fb,time) VALUES (?,?,?)',(username,feedback,now))
+	c.execute('INSERT INTO fbf (username,fb,time) VALUES (?,?,?)',(username,feedback,str(now)))
 	conn.commit()
 def login_user(username,password):
 	c.execute('SELECT * FROM final WHERE username =? AND password = ?',(username,password))
@@ -75,6 +90,8 @@ def takemail(username):
 	return li[0][2]
 
 
+from streamlit_option_menu import option_menu
+
 with open("LASSO.txt",'rb')as f:
     model1 = pickle.load(f)
 
@@ -83,6 +100,16 @@ with open("RIDGE.txt", 'rb') as f:
 
 with open("LINEAR.txt",'rb')as f:
     model3 = pickle.load(f)
+
+with open("cclinear.txt",'rb')as f:
+    cclinear = pickle.load(f)
+
+with open("cclasso.txt",'rb')as f:
+    cclasso = pickle.load(f)
+
+with open("ccridge.txt",'rb')as f:
+    ccridge = pickle.load(f)
+
 
 
 from email.message import EmailMessage
@@ -93,8 +120,8 @@ def otpmailing(con,mail):
 	email_password = "qtlvbndfudxrvqrb"
 	email_receiver = mail
 
-	subject = "Authentication for codeforces predictor application login"
-	body = str(con) + str(" - is the One Time Password for your login access... Hope you will enjoy my app...")
+	subject = "Authentication OTP for your login on CP predictor"
+	body = str(con)
 
 	em = EmailMessage()
 	em['From'] = email_sender
@@ -126,14 +153,29 @@ def prediction(div,rank,rating):
 	st.success("Ridge  Model  :"+str(model2.predict([[div,rank,rating]])))
 	st.success("Linear Model  :"+str(model3.predict([[div,rank,rating]])))
 
+def predictioncc(rank,rating):
+	rank = float(rank)
+	rating =float(rating)
+	st.success("Lasso  Model  :"+str(cclasso.predict([[rank,rating]])))
+	st.success("Ridge  Model  :"+str(ccridge.predict([[rank,rating]])))
+	st.success("Linear Model  :"+str(cclinear.predict([[rank,rating]])))
+
+import requests
+
+def load_lottieurl(url : str):
+	r = requests.get(url)
+	if r.status_code !=200:
+		return None
+	return r.json()
+
+
 
 
 def main(data):
 	print("HOME")
-	#create_usertable()
+	create_usertable()
 	if selected == "Home":
-		st.subheader("CODEFORCES RATING PREDICTOR")
-
+		st.subheader("CODING PLATFORMS RATING PREDICTOR")
 		menu = ["Login", "SignUp","Logout"]
 		choice = st.selectbox("Menu", menu)
 		if(choice == 'Login'):
@@ -167,6 +209,17 @@ def main(data):
 						pass
 					file.close()
 			if(st.checkbox("Forget Password")):
+				lottie_hello = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_73lUtMVj1h.json")
+				st_lottie(
+					lottie_hello,
+					speed=1,
+					reverse=False,
+					loop=True,
+					quality="low",  # medium ; high
+					height=None,
+					width=None,
+					key=None,
+				)
 				mail = st.text_input("Please enter your registered mail:")
 				if(st.checkbox("Confirm")):
 					try:
@@ -184,7 +237,7 @@ def main(data):
 							# st.write("Inside the form")
 							otp = st.text_input("OTP")
 						# Every form must have a submit button
-							passval = st.text_input("New Password",type='password')
+							passval = st.text_input("New Password")
 							submitted = st.button("Submit")
 							if submitted:
 								with open('otp2.txt', 'r') as f:
@@ -200,7 +253,7 @@ def main(data):
 									print('data')
 									passval=str(passval)
 									update(username, make_hashes(passval), mail)
-									st.write("Remember your password : " + passval)
+									st.write(passval)
 									st.success("You have successfully changed your password")
 									print('yes')
 									st.info("you can now login again with your new password")
@@ -217,6 +270,17 @@ def main(data):
 
 
 		elif choice == "SignUp":
+			lottie_hello = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_QFWqUliTKj.json")
+			st_lottie(
+				lottie_hello,
+				speed=1,
+				reverse=False,
+				loop=True,
+				quality="low",  # medium ; high
+				height=None,
+				width=None,
+				key=None,
+			)
 			st.subheader("Create New Account")
 			new_user = st.text_input("Username")
 			new_mail = st.text_input("Mail")
@@ -268,11 +332,17 @@ def main(data):
 					st.warning("Username not valid - Already taken by someone")
 
 		elif choice == "Logout":
-			#st.subheader("User Profiles")
-			#user_result = view_all_users()
-			#clean_db = pd.DataFrame(user_result, columns=["Username", "Password", "Mail"])
-			#print(pd)
-			#st.dataframe(clean_db)
+			lottie_hello = load_lottieurl("https://assets4.lottiefiles.com/packages/lf20_VBz0xZZjd6.json")
+			st_lottie(
+				lottie_hello,
+				speed=1,
+				reverse=False,
+				loop=True,
+				quality="low",  # medium ; high
+				height=None,
+				width=None,
+				key=None,
+			)
 			st.subheader("Are you sure that you want to quit....")
 			if(st.button("Confirm")):
 				st.success("You have logged out successfully")
@@ -285,13 +355,41 @@ def main(data):
 				file.close()
 	if selected == "Predict":
 		if int(data)==1:
-			division = st.text_input("Division")
-			rank = st.text_input("Rank")
-			curr_rat = st.text_input("Current Rating")
-			if(st.button("Predict")):
-				#try:
-				prediction(division,rank,curr_rat)
-				st.balloons()
+			with st.sidebar:
+				add_radio = st.radio(
+					"Choose a coding platform",
+					("CodeForces", "CodeChef")
+				)
+				try:
+					if add_radio == 'CodeForces':
+						division = st.text_input("Division")
+						rank = st.text_input("Rank")
+						curr_rat = st.text_input("Current Rating")
+						if (st.button("Predict")):
+							# try:
+							prediction(division, rank, curr_rat)
+							st.balloons()
+					else:
+						rank = st.text_input("Rank")
+						curr_rat = st.text_input("Current Rating")
+						if (st.button("Predict")):
+						# try:
+							predictioncc(rank, curr_rat)
+							st.balloons()
+				except:
+					st.warning("OOPS :) Something went wrong...!")
+			lottie_hello = load_lottieurl("https://assets3.lottiefiles.com/packages/lf20_ZsO4t2Bow2.json")
+			st_lottie(
+				lottie_hello,
+				speed=1,
+				reverse=False,
+				loop=True,
+				quality="low",  # medium ; high
+				height=None,
+				width=None,
+				key=None,
+			)
+
 				#except:
 					#st.warning("Please... fill valid details")
 		else :
@@ -299,8 +397,8 @@ def main(data):
 
 	elif selected == "Contact":
 		if int(data) == 1:
-			st.subheader("Share your feedback ... !")
-			text = st.text_area("Enter your feedback for my application : ")
+			st.subheader("Wanakkam nanba ... !")
+			text = st.text_area("Enter your feedback of our application : ")
 			if(st.button("Submit FeedBack")):
 				with open("uname.txt", 'r') as file:
 					username = file.read()
@@ -311,8 +409,27 @@ def main(data):
 		else :
 			st.warning("You have to login First")
 
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://kindertravelguide.com/wp-content/uploads/2015/12/login-background.jpg");
+background-size: 180%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 if __name__ == '__main__':
-	#c.execute('CREATE TABLE fbf(username TEXT,fb TEXT ,time TEXT);')
+
 	with st.spinner("Please Wait..."):
 		with open('flag.txt', 'r') as f:
 			data = f.read()
